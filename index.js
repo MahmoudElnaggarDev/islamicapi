933b6255ebbe83c686652657dbb4e98edde68fbd
MahmoudElnaggarDev <139114446+MahmoudElnaggarDev@users.noreply.github.com>
2023-12-06 09:07:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 app.listen(8080);
 app.get("/message", (req, res) => {
-    const messages = ["﴿ مَنْ كَانَ يُرِيدُ ثَوَابَ الدُّنْيَا فَعِنْدَ اللَّهِ ثَوَابُ الدُّنْيَا وَالْآخِرَةِ وَكَانَ اللَّهُ سَمِيعًا بَصِيرًا ﴾ [النساء: ١٣٤] "];
+    const messages = ["﴿ مَنْ كَانَ يُرِيدُ ثَوَابَ الدُّنْيَا فَعِنْدَ اللَّهِ ثَوَابُ الدُّنْيَا وَالْآخِرَةِ وَكَانَ اللَّهُ سَمِيعًا بَصِيرًا ﴾ [النساء: ١٣٤] ", "الله يراك", "إن الله يحب التوابين",];
     const random = Math.floor(Math.random() * messages.length);
     res.status(200).send({
         text: messages[random]
