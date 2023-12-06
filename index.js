cb1a9602857054e5b2c1043bbc6ae8fdad51a122
MahmoudElnaggarDev <139114446+MahmoudElnaggarDev@users.noreply.github.com>
2023-12-05 20:11:09 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+app.listen(8080);
 app.get("/message", (req, res) => {
     const messages = [""];
     const random = Math.floor(Math.random() * messages.length);
