1dfa9c1d755548c92a5cb2aa9c2ba3bf32c8efed
MahmoudElnaggarDev <139114446+MahmoudElnaggarDev@users.noreply.github.com>
2023-12-05 19:50:26 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("express")();
 const port = 8080;
-app.listen(port)
-app.get("/test", (req, res) => {
+app.listen(port);
+app.get("/messages", (req, res) => {
     const messages = [""];
     const random = Math.floor(Math.random() * messages.length);
     res.status(200).send({
         text: messages[random]
-    })
-})
+    });
+});
